50707ee4e4fdfcaaaa2f125e3768bea3a023c187
Liz Favre <egfavre@gmail.com>
2016-06-02 11:01:12 -0400

    initial import

---
+++ desktop/src/com/egfavre/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.egfavre.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.egfavre.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 800;
+		config.width = 1200;
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
